d7031242df4e01f8dd74c2f8e27422b04397a0c5
hugeek <hugui_95@hotmail.com>
2019-08-31 12:01:28 -0500

    inicio

---
+++ assets/js/home.js
@@ -0,0 +1,29 @@
+let frutas = ["mango", "cereza", "manzana", "uva", "pera", "sandía"];
+
+/* Arreglos */
+/*
+      let calificaciones = [10, 5, 6.7, 8, 5.9];
+
+      for (let i = 0; i < frutas.length; i++) {
+        console.log(frutas[i]);
+      }
+      
+      for (let i = 0; i < calificaciones.length; i++) {
+        console.log(calificaciones[i]);
+        if (calificaciones[i] >= 6) {
+          console.log("¡Aprobado!");
+        } else {
+          console.log("¡Reprobado!");
+        }
+      }
+    
+      frutas.push("limón");
+
+      for (let i = 0; i < frutas.length; i++) {
+        console.log(frutas[i]);
+      }  */
+
+/// Objeto
+let producto = { nombre: "Mango", peso: "1kg", costo: "$20" };
+
+console.log(producto.nombre, producto.peso, producto.costo);
